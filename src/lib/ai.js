8019e72bbdd3c035a15734c1f8c40dd7826c1734
jakhongirov <johongirovdiyorbek08@gmail.com>
2025-04-26 17:42:20 +0800

    add

--- src/lib/ai.js
+++ src/lib/ai.js
@@ -12,52 +12,52 @@ const analyzeText = async (inputText, categories) => {
    try {
       const current_datetime = getFormattedDate()
       const prompt = `
-         You are an AI that extracts structured financial details from a text statement.
+         ...You are an AI that extracts structured financial details from a text statement.
 
          ## Task:
          Analyze the input statement and return ONLY a JSON object with these keys:
          - "user_input": The original user input as a string.
-         - "category": The type of expense or income (e.g., "Food", "Salary", "Rent").
+         - "category": The category of expense or income flow categories list below provided. 
          - "amount": The numerical value of the transaction.
-         - "currency": The currency used in the transaction (e.g., "USD", "EUR", "UZS"). If the currency is not explicitly mentioned, infer from the context or default to "USD".
-         - "type": Either "income" or "outcome" based on whether it's earnings or spending.
-         - "date": The date and time of the transaction in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-15 20:34:59.25447+05). If the date and time are not explicitly mentioned, use the provided current date and time: ${current_datetime}. If the input text contains a date, overwrite the current date with the found date.
+         - "currency": The currency used in the transaction (e.g., "USD", "EUR", "UZS"). If the currency is not explicitly mentioned, think, if amount more than "30000" maybe it is "UZS", another cases maybe - "USD" 
+         - "type": Either "income" for earning or "outcome" for spending - based on whether it's income or outcome.
+         - "date": The date and time of the transaction in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-15 20:34:59.25447+05). If the date and time are not explicitly mentioned, use the provided current date and time: ${current_datetime}. If the input contains a date, overwrite the current date with the found date.
          - "deadline": The date and time the debt is expected to be repaid in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-21 20:34:59.25447+05). If no deadline is mentioned, set it to "".
          - "isDebtPayment": true if the transaction is a debt payment, false otherwise.
          - "forWhom": The name of the person or entity involved in the debt payment. If not a debt payment, set it to "".
 
-         ## Categories:
-         ${categories}
+         ## Categories list - use it for clarify category of transaction:
+         ${categories} . For the transaction category, strictly choose the closest or most accurate one from the available categories list. If none match, select the "Other expenses" category.
 
          ## Examples:
          1. Input: "I paid $200 for my rent on 2024-06-15 10:00:00+05"
             Output: [{{"user_input": "I paid $200 for my rent on 2024-06-15 10:00:00+05", "isDebtPayment": false, "forWhom": "", "category": "Uy-joy xarajatlari", "amount": 200, "currency": "USD", "type": "outcome", "date": "2024-06-15 10:00:00.000000+05", "deadline": ""}}]
 
-         2. Input: "I borrowed 500$ from Alex aka yesterday, he will return it on 2024-06-25 12:00:00+05"
-            Output: [{{"user_input": "I borrowed 500$ from Alex aka yesterday, he will return it on 2024-06-25 12:00:00+05", "isDebtPayment": true, "forWhom": "Alex aka", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2024-06-25 12:00:00.000000+05"}}]
+         2. Input: "Aziz akaga kecha 500$ qarz berdim, 25 iyunda qaytarishi kerak"
+            Output: [{{"user_input": "Aziz akaga kecha 500$ qarz berdim, 25 iyunda qaytarishi kerak", "isDebtPayment": true, "forWhom": "Alex aka", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2024-06-25 12:00:00.000000+05"}}]
 
-         3. Input: "I received my salary of 1000 UZS"
-            Output: [{{"user_input": "I received my salary of 1000 UZS", "isDebtPayment": false, "forWhom": "", "category": "Mehnat daromadlari", "amount": 1000, "currency": "UZS", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+         3. Input: "5000000 oylik oldim"
+            Output: [{{"user_input": "5000000 oylik oldim", "isDebtPayment": false, "forWhom": "", "category": "Mehnat daromadlari", "amount": 5000000, "currency": "UZS", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
-         4. Input: "I spent 50 sum on dinner at a restaurant"
-            Output: [{{"user_input": "I spent 50 sum on dinner at a restaurant", "isDebtPayment": false, "forWhom": "", "category": "Oziq-ovqat", "amount": 50, "currency": "UZS", "type": "outcome", "date": "${current_datetime}", "deadline": ""}}]
+         4. Input: "Obed uchun ovqatlangani 50000 ishlatdim"
+            Output: [{{"user_input": "Obed uchun ovqatlangani 50000 ishlatdim", "isDebtPayment": false, "forWhom": "", "category": "Oziq-ovqat", "amount": 50000, "currency": "UZS", "type": "outcome", "date": "${current_datetime}", "deadline": ""}}]
 
-         5. Input: "Nodir $200 qarzini berdi kecha"
-            Output: [{{"user_input": "Nodir $200 qarzini berdi kecha", "isDebtPayment": true, "forWhom": "Nodir", "category": "Moliyaviy majburiyatlar", "amount": 200, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+         5. Input: "Nodir $200 qarzini berdi"
+            Output: [{{"user_input": "Nodir $200 qarzini berdi", "isDebtPayment": true, "forWhom": "Nodir", "category": "Qarzlar", "amount": 200, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
          6. Input: "Kecha anvar 100$ qarzini berdi"
-            Output: [{{"user_input": "Kecha anvar 100$ qarzini berdi", "isDebtPayment": true, "forWhom": "Anvar", "category": "Moliyaviy majburiyatlar", "amount": 100, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+            Output: [{{"user_input": "Kecha anvar 100$ qarzini berdi", "isDebtPayment": true, "forWhom": "Anvar", "category": "Qarzlar", "amount": 100, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
-         7. Input: "Anvarga qarz berdim 500 dollar. 21-mart 12:00:00+05 da qaytaradi."
-            Output: [{{"user_input": "Anvarga qarz berdim 500 dollar. 21-mart 12:00:00+05 da qaytaradi.", "isDebtPayment": true, "forWhom": "Anvar", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2025-03-21 12:00:00.000000+05"}}]
+         7. Input: "Anvarga qarz berdim 500. 21-martda qaytaradi."
+            Output: [{{"user_input": "Anvarga qarz berdim 500. 21-martda qaytaradi.", "isDebtPayment": true, "forWhom": "Anvar", "category": "Qarzlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2025-03-21 12:00:00.000000+05"}}]
 
          ## Now, process this input:
          "${inputText}"
 
-         ## Input text only in Uzbek, English and Russian languages.
 
+         ## Input text only in Uzbek, English and Russian languages.
          ## Output ONLY JSON. No explanations, no text, just JSON. If the input text is not suitable for financial statement extraction, output 'wrong'.
-         ## Ensure that the 'category' field is ONLY populated with values provided in the '## Categories:' section.
+         ...
       `;
 
       const result = await model.generateContent([prompt]);
@@ -109,48 +109,48 @@ const analyzeVoice = async (tempFilePath, categories) => {
          ## Task:
          Analyze the input statement and return ONLY a JSON object with these keys:
          - "user_input": The original user input as a string.
-         - "category": The type of expense or income (e.g., "Food", "Salary", "Rent").
+         - "category": The category of expense or income flow categories list below provided. 
          - "amount": The numerical value of the transaction.
-         - "currency": The currency used in the transaction (e.g., "USD", "EUR", "UZS"). If the currency is not explicitly mentioned, infer from the context or default to "USD".
-         - "type": Either "income" or "outcome" based on whether it's earnings or spending.
-         - "date": The date and time of the transaction in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-15 20:34:59.25447+05). If the date and time are not explicitly mentioned, use the provided current date and time: ${current_datetime}. If the input text contains a date, overwrite the current date with the found date.
+         - "currency": The currency used in the transaction (e.g., "USD", "EUR", "UZS"). If the currency is not explicitly mentioned, think, if amount more than "30000" maybe it is "UZS", another cases maybe - "USD" 
+         - "type": Either "income" for earning or "outcome" for spending - based on whether it's income or outcome.
+         - "date": The date and time of the transaction in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-15 20:34:59.25447+05). If the date and time are not explicitly mentioned, use the provided current date and time: ${current_datetime}. If the input contains a date, overwrite the current date with the found date.
          - "deadline": The date and time the debt is expected to be repaid in "YYYY-MM-DD HH:MM:SS.mmmmmm+ZZ" format (e.g., 2025-03-21 20:34:59.25447+05). If no deadline is mentioned, set it to "".
          - "isDebtPayment": true if the transaction is a debt payment, false otherwise.
          - "forWhom": The name of the person or entity involved in the debt payment. If not a debt payment, set it to "".
 
-         ## Categories:
-        ${categories}
+         ## Categories list - use it for clarify category of transaction:
+         ${categories} . For the transaction category, strictly choose the closest or most accurate one from the available categories list. If none match, select the "Other expenses" category.
 
          ## Examples:
          1. Input: "I paid $200 for my rent on 2024-06-15 10:00:00+05"
             Output: [{{"user_input": "I paid $200 for my rent on 2024-06-15 10:00:00+05", "isDebtPayment": false, "forWhom": "", "category": "Uy-joy xarajatlari", "amount": 200, "currency": "USD", "type": "outcome", "date": "2024-06-15 10:00:00.000000+05", "deadline": ""}}]
 
-         2. Input: "I borrowed 500$ from Alex aka yesterday, he will return it on 2024-06-25 12:00:00+05"
-            Output: [{{"user_input": "I borrowed 500$ from Alex aka yesterday, he will return it on 2024-06-25 12:00:00+05", "isDebtPayment": true, "forWhom": "Alex aka", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2024-06-25 12:00:00.000000+05"}}]
+         2. Input: "Aziz akaga kecha 500$ qarz berdim, 25 iyunda qaytarishi kerak"
+            Output: [{{"user_input": "Aziz akaga kecha 500$ qarz berdim, 25 iyunda qaytarishi kerak", "isDebtPayment": true, "forWhom": "Alex aka", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2024-06-25 12:00:00.000000+05"}}]
 
-         3. Input: "I received my salary of 1000 UZS"
-            Output: [{{"user_input": "I received my salary of 1000 UZS", "isDebtPayment": false, "forWhom": "", "category": "Mehnat daromadlari", "amount": 1000, "currency": "UZS", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+         3. Input: "5000000 oylik oldim"
+            Output: [{{"user_input": "5000000 oylik oldim", "isDebtPayment": false, "forWhom": "", "category": "Mehnat daromadlari", "amount": 5000000, "currency": "UZS", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
-         4. Input: "I spent 50 sum on dinner at a restaurant"
-            Output: [{{"user_input": "I spent 50 sum on dinner at a restaurant", "isDebtPayment": false, "forWhom": "", "category": "Oziq-ovqat", "amount": 50, "currency": "UZS", "type": "outcome", "date": "${current_datetime}", "deadline": ""}}]
+         4. Input: "Obed uchun ovqatlangani 50000 ishlatdim"
+            Output: [{{"user_input": "Obed uchun ovqatlangani 50000 ishlatdim", "isDebtPayment": false, "forWhom": "", "category": "Oziq-ovqat", "amount": 50000, "currency": "UZS", "type": "outcome", "date": "${current_datetime}", "deadline": ""}}]
 
-         5. Input: "Nodir $200 qarzini berdi kecha"
-            Output: [{{"user_input": "Nodir $200 qarzini berdi kecha", "isDebtPayment": true, "forWhom": "Nodir", "category": "Moliyaviy majburiyatlar", "amount": 200, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+         5. Input: "Nodir $200 qarzini berdi"
+            Output: [{{"user_input": "Nodir $200 qarzini berdi", "isDebtPayment": true, "forWhom": "Nodir", "category": "Qarzlar", "amount": 200, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
          6. Input: "Kecha anvar 100$ qarzini berdi"
-            Output: [{{"user_input": "Kecha anvar 100$ qarzini berdi", "isDebtPayment": true, "forWhom": "Anvar", "category": "Moliyaviy majburiyatlar", "amount": 100, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
+            Output: [{{"user_input": "Kecha anvar 100$ qarzini berdi", "isDebtPayment": true, "forWhom": "Anvar", "category": "Qarzlar", "amount": 100, "currency": "USD", "type": "income", "date": "${current_datetime}", "deadline": ""}}]
 
-         7. Input: "Anvarga qarz berdim 500 dollar. 21-mart 12:00:00+05 da qaytaradi."
-            Output: [{{"user_input": "Anvarga qarz berdim 500 dollar. 21-mart 12:00:00+05 da qaytaradi.", "isDebtPayment": true, "forWhom": "Anvar", "category": "Moliyaviy majburiyatlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2025-03-21 12:00:00.000000+05"}}]
+         7. Input: "Anvarga qarz berdim 500. 21-martda qaytaradi."
+            Output: [{{"user_input": "Anvarga qarz berdim 500. 21-martda qaytaradi.", "isDebtPayment": true, "forWhom": "Anvar", "category": "Qarzlar", "amount": 500, "currency": "USD", "type": "outcome", "date": "${current_datetime}", "deadline": "2025-03-21 12:00:00.000000+05"}}]
 
          ## Now, process this input:
          "${await getTextFromAudio(uploadResult.file.uri)}"
 
+
          ## Input text only in Uzbek, English and Russian languages.
-         
          ## Output ONLY JSON. No explanations, no text, just JSON. If the input text is not suitable for financial statement extraction, output 'wrong'.
-         ## Ensure that the 'category' field is ONLY populated with values provided in the '## Categories:' section.
       `;
+
 
       const result = await model.generateContent([prompt]);
       const responseText = result.response.text();
